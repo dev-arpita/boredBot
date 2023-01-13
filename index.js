74b38ad531cb357b418d938c52a35e05a557668b
Arpita Roy <arpita.roy.workar@gmail.com>
2023-01-13 10:41:00 +0530

    BoredBot- Extra Styling

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 /**
 Challenge:
 
-When the button is clicked, call out to the Bored API
-(URL: https://apis.scrimba.com/bored/api/activity)
-and replace the h4 with the activity from the API
-
+- Make the styling more exciting once an activity idea comes
+back from the Bored API
+    - Resources: DOM element "classList" property, uigradients.com,
+      Google Fonts, color.adobe.com
+    - Some ideas:
+      - Change the title from "BoredBot" to something more exciting!
+      - Change the background to something less drab.
+      - Bonus: Animate something on the screen to move around and add more
+        excitement to the page
 */
 
-function showActivity() {
+document.getElementById("get-activity").addEventListener("click", function() {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("activity").textContent = data.activity
+      document.body.classList.add("clr-bg")
+      document.getElementById("title").innerHTML = `
+        JumpingBot
+      <span>
+      <img src="img/jumpingBot.png"/>
+      </span>
 
-fetch("https://apis.scrimba.com/bored/api/activity")
-  .then(response => response.json())
-  .then(data =>
-     document.getElementById("activity").textContent = data.activity
-)
-
-
-}
-
-
-  document.getElementById("btn").addEventListener("click", (showActivity))
+      `
+    })
+})
